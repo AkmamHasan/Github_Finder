b8f6cefe8b4ae8c4738977d221c30c589c5fc9c4
Akmam Hasan <akmam.hasan@northsouth.edu>
2023-10-06 14:29:32 +0600

    Add files via upload

---
+++ github-finder/app.js
@@ -0,0 +1,33 @@
+// Init Github
+const github = new Github;
+
+// Init UI
+const ui = new UI;
+
+// search input
+const searchUser = document.getElementById('searchUser');
+
+// Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+    // Get input text
+    const userText = e.target.value;
+
+    if(userText !== ''){
+       // Make http calls
+       github.getUser(userText)
+        .then(data => {
+            if(data.profile.message === 'Not Found') {
+                // show alert
+                ui.showAlert('User not found', 'alert alert-danger');
+
+            } else {
+                // show profile
+                ui.showProfile(data.profile);
+                ui.showRepos(data.repos);
+            }
+        }) 
+    } else {
+        // clear profile
+        ui.clearProfile();
+    }
+});
